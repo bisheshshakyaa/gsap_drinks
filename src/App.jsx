0eca36f1b7a9df7db5217a4a4f10a3b387e6ea98
bisheshshakyaa <bisheshshakya123@gmail.com>
2025-09-19 15:46:44 +0545

    Menu page add need some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/hero";
 import Cocktails from "./components/cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </main>
   );
 };
